c692862d7814f4d55d584dfac7690ee1bfecb97d
Chris <chris_waity@126.com>
2019-10-15 11:36:18 -0400

    delete local file

--- pp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-kubectl delete -f examples/vxlan/
-rm /var/lib/cni/networks/vxlan1/* -rf
-ETCDCTL_API=3 etcdctl --endpoints="192.168.56.201:12379" del multus --prefix
-scp ./bin/* 192.168.56.202:/opt/cni/bin/
-bin/daemon
-
--- tor.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-scp bin/* 119.3.241.161:/opt/cni/bin/
-scp bin/* 119.3.247.220:/opt/cni/bin/
